a0e86f39888fb034e540f59acda53c36244a7f1c
“HazorTremz” <himanshu.s.rawat2001@gmail.com>
2024-01-22 21:44:40 +0530

    adding files

--- eval.py
+++ eval.py
@@ -9,18 +9,18 @@ import os
 import json
 import argparse
 import torchvision.models as models
+from metrics import (
+    accuracy,
+    precision,
+    recall,
+    f1_score,
+    specificity,
+)
 
 def load_and_convert_model(model_path, input_shape, precision=torch.half):
-    # Load PyTorch model
-    #model = torch.load(model_path).to("cuda")
-
-    # Trace the model
     resnet18 = models.resnet18(pretrained=True).half().to("cuda")
-    #traced_model = torch.jit.trace(resnet18, torch.randn(input_shape).to("cuda"))
-   
-    #traced_model=torch.jit.load(model_path).to("cuda")
     inputs = [torch.randn((1, 3, 224, 224)).to("cuda").half()]
-    # Convert to TensorRT format
+
     trt_model = torch_tensorrt.compile(
         resnet18,
         inputs=inputs,
@@ -29,59 +29,82 @@ def load_and_convert_model(model_path, input_shape, precision=torch.half):
 
     return trt_model
 
-def inference(trt_model, img_batch, topk=5):
+def inference(trt_model, img, topk=5):
     trt_model.eval()
     with torch.no_grad():
-        outputs = trt_model(img_batch)
-    prob = torch.nn.functional.softmax(outputs[0], dim=1)
+        outputs = trt_model(img)
 
+    prob = torch.nn.functional.softmax(outputs[0], dim=1)
     probs, classes = torch.topk(prob, topk, dim=1)
+
     return probs, classes
 
-def preprocess_image(img_dir,img_path):
-    img_path=os.path.join(img_dir, img_path)
+
+def preprocess_image(img_dir, img_path):
+    img_path = os.path.join(img_dir, img_path)
     image = Image.open(img_path).convert("RGB")
     input_image = image.resize((224, 224))
     input_image_array = np.array(input_image)
     img = transforms.ToTensor()(input_image_array).unsqueeze(0).to("cuda")
     return img
 
-def main(model_path, img_dir,categories_path):
+def evaluate_images_tensorrt(trt_model, split_path, labels):
+    trt_model.eval()
+    
+    true_labels = []
+    predicted_labels = []
+
+    for class_folder in os.listdir(split_path):
+        class_path = os.path.join(split_path, class_folder)
+
+        for img_path in os.listdir(class_path):
+            input_img = preprocess_image(class_path, img_path).half()
+            print(f"Input image shape: {input_img.shape}") 
+            probs, classes = inference(trt_model, input_img, topk=1)
+
+            predicted_idx = int(classes[0])
+            predicted_label = labels[predicted_idx]
+            print(predicted_idx, predicted_label)
+            predicted_labels.append(predicted_idx)
+            
+            true_label = class_folder 
+            true_labels.append(true_label)
+            true_labels=[564,756] #just dummy values 
+
+    numeric_actual_labels = np.array(true_labels)
+    numeric_predicted_labels = np.array(predicted_labels).reshape(1, -1)
+    print(numeric_predicted_labels.shape)
+
+    true_labels = torch.tensor(numeric_actual_labels).view(1, -1)[:, 0].view(-1)
+    predicted_labels = torch.tensor(numeric_predicted_labels)
+
+    metrics = {}
+    metrics['acc1'] = accuracy(predicted_labels, true_labels, topk=(1,))[0]
+    metrics['precision'] = precision(predicted_labels, true_labels)
+    metrics['recall'] = recall(predicted_labels, true_labels)
+    metrics['f1_score'] = f1_score(predicted_labels, true_labels)
+    metrics['specificity'] = specificity(predicted_labels, true_labels)
+
+    for metric_name, metric_value in metrics.items():
+        print(f"{metric_name}: {metric_value}")
+
+def main():
+    # Set paths relative to the current directory
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(current_dir, "resnet18.pt")
+    split_path = os.path.join(current_dir, "images/test")
+    categories_path = os.path.join(current_dir, "imagenet.json")
+
     # Load and convert the model
-    input_shape = (1, 3, 224, 224)  # Adjust the input shape based on your model
+    input_shape = (1, 3, 224, 224)
     trt_model = load_and_convert_model(model_path, input_shape)
 
-    # Load the batch of images
-    img_batch = torch.cat([preprocess_image(img_dir,img_path).half() for img_path in os.listdir(img_dir)])
-
-    # Run inference
-    topk = 5  # Number of top predictions
-    probs, classes = inference(trt_model, img_batch, topk)
-    
+    # Load labels
     with open(categories_path, 'r') as f:
         categories = json.load(f)
-    # Display results
-    # Replace with your class labels
-    for i in os.listdir(img_dir):
-        print(f"Results for {i}:")
-        for j in range(topk):
-            probability = probs[j].item()
-            class_index = int(classes[i][j])
-        
-        # Get label from 'categories' dictionary
-            if str(class_index) in categories:
-                class_label = categories[str(class_index)][1]
-            else:
-                class_label = "Unknown"
-        print(f"Top {j + 1}: {class_label} - Probability: {probability * 100:.2f}%")
+
+    # Evaluate images
+    evaluate_images_tensorrt(trt_model, split_path, categories)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--model_path", type=str, help="Path to the model")
-    parser.add_argument("--img_dir", type=str, help="Path to the image folder")
-    parser.add_argument("--categories_path", type=str, help="Path to the JSON file with categories")
-
-    args = parser.parse_args()
-
-    main(args.model_path, args.img_dir, args.categories_path)
+    main()
